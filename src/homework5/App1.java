bf5a69dc568d2c254c6acdef3df91ef3af6c8433
Serj <serj388@gmail.com>
2022-09-27 08:28:34 +0300

    Created app with string

--- src/homework5/App1.java
+++ src/homework5/App1.java
@@ -1,4 +1,16 @@
 package homework5;
 
 public class App1 {
+
+    public static void main(String[] args) {
+        String str = "ZюукяДZіботZ,ежобZощZяZенZ!ьлаксом";
+
+        String[] words = new StringBuilder(str).reverse().toString().split("Z");
+        StringBuilder notMoskal = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            notMoskal.append(words[i]).append(" ");
+        }
+
+        System.out.println(notMoskal);
+    }
 }

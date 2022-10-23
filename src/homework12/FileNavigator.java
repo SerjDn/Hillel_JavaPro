f7c92b12bed211d6b022b5cadccb7a6bee48dc35
Serj <serj388@gmail.com>
2022-10-23 14:24:54 +0300

    added some methods

--- src/homework12/FileNavigator.java
+++ src/homework12/FileNavigator.java
@@ -9,15 +9,35 @@ public class FileNavigator {
     Map<String, ArrayList<FileData>> fileStorage = new HashMap<>();
 
     public void add(String url, FileData fileData) {
-
         if (!fileStorage.containsKey(url)) {
             fileStorage.put(url, new ArrayList<FileData>());
             fileStorage.get(url).add(fileData);
         } else {
             fileStorage.get(url).add(fileData);
         }
-
         System.out.println(fileStorage.get(url));
     }
 
+    public void find(String url) {
+        if (fileStorage.containsKey(url)) {
+            ArrayList<FileData> values = new ArrayList<>(fileStorage.get(url));
+            System.out.println(fileStorage.get(url));
+            System.out.println(values);
+
+            System.out.println();
+        } else {
+            System.out.println("нету");
+        }
+    }
+
+    public void filterBySize(int size) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "FileNavigator{" +
+                "fileStorage=" + fileStorage +
+                '}';
+    }
 }
--- src/homework12/Main.java
+++ src/homework12/Main.java
@@ -1,19 +1,22 @@
 package homework12;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        FileData fileData = new FileData("Homework", 10, "C://Hillel");
-        FileData fileData2 = new FileData("Lessons", 15, "C://Hillel");
-        FileData fileData3 = new FileData("Practice", 20, "C://Hillel//Practice");
+        FileData fileData = new FileData("Homework", 10, "C:/Hillel/Lesson1");
+        FileData fileData2 = new FileData("Lessons", 15, "C:/Hillel/Lesson2");
+        FileData fileData3 = new FileData("Practice", 20, "C:/Practice/Homework1");
 
         FileNavigator fileNavigator = new FileNavigator();
-        fileNavigator.add("C://Hillel", fileData3);
-        fileNavigator.add("C://Hillel", fileData2);
-        fileNavigator.add("C://Hillel//Practice", fileData);
+        fileNavigator.add("C:/Hillel", fileData3);
+        fileNavigator.add("C:/Hillel", fileData2);
+        fileNavigator.add("C:/Practice", fileData);
+
+        System.out.println("================================================\n"
+                + fileNavigator + "\n================================================");
+
+        fileNavigator.find("C:/Hillel");
 
     }
 

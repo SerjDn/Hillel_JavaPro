c5259664c6a632745948e5ae730129ab8c2a5527
Serj <serj388@gmail.com>
2022-10-24 23:01:14 +0300

    added some logic

--- src/homework12/FileNavigator.java
+++ src/homework12/FileNavigator.java
@@ -1,8 +1,6 @@
 package homework12;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class FileNavigator {
 
@@ -10,28 +8,82 @@ public class FileNavigator {
 
     public void add(String url, FileData fileData) {
         if (!fileStorage.containsKey(url)) {
-            fileStorage.put(url, new ArrayList<FileData>());
-            fileStorage.get(url).add(fileData);
-        } else {
-            fileStorage.get(url).add(fileData);
+            fileStorage.put(url, new ArrayList<>());
         }
-        System.out.println(fileStorage.get(url));
+        fileStorage.get(url).add(fileData);
     }
 
-    public void find(String url) {
+    public ArrayList<FileData> find(String url) {
         if (fileStorage.containsKey(url)) {
             ArrayList<FileData> values = new ArrayList<>(fileStorage.get(url));
-            System.out.println(fileStorage.get(url));
-            System.out.println(values);
-
-            System.out.println();
+            return values;
         } else {
-            System.out.println("нету");
+            System.out.println("There are no files...");
         }
+        return null;
     }
 
-    public void filterBySize(int size) {
+    public ArrayList<FileData> filterBySize(int size) {
+        ArrayList<FileData> valueList = new ArrayList<>();
 
+        for (ArrayList<FileData> entry : fileStorage.values()) {
+            for (int i = 0; i < entry.size(); i++) {
+                valueList.add(entry.get(i));
+            }
+        }
+
+        System.out.println("valueList is:\n" + valueList);
+        System.out.println("size = " + size);
+        System.out.println("valueList after filter is:");
+
+        for (int i = 0; i < valueList.size(); i++) {
+            if (valueList.get(i).getSize() > size) {
+                valueList.remove(i);
+                i--;
+            }
+        }
+
+        if (valueList.size() == 0) {
+            System.out.println("There are no files...");
+        }
+        return valueList;
+    }
+
+//    private void filterBySizeTest() {
+//        ArrayList valueList = new ArrayList<>();
+//        for (Map.Entry entry : fileStorage.entrySet()) {
+//            System.out.println(entry);
+//            System.out.println(entry.getKey() + " | " + entry.getValue());
+//            valueList.add(entry.getValue());
+//        }
+//        System.out.println("------------------------------");
+//        ArrayList<FileData> fileData = new ArrayList<>(valueList);
+//        for (int i = 0; i < fileData.size(); i++) {
+//            System.out.println(fileData.get(i).getPath());
+//        }
+//    }
+
+    public void remove(String path) {
+        ArrayList<FileData> valueList = new ArrayList<>();
+        ArrayList<FileData> valueListToDel = new ArrayList<>();
+
+        for (ArrayList<FileData> entry : fileStorage.values()) {
+            for (int i = 0; i < entry.size(); i++) {
+                if (!entry.get(i).getPath().equals(path)) {
+                    valueList.add(entry.get(i));
+                } else {
+                    valueListToDel.add(entry.get(i));
+                }
+            }
+        }
+
+        System.out.println("New valueList without <<" + path + ">> is: \n" + valueList);
+        System.out.println("New valueList with <<" + path + ">> is: \n" + valueListToDel);
+
+        for (Map.Entry entry : fileStorage.entrySet()) {
+            System.out.println("key + value:");
+            System.out.println(entry.getKey() + " + " + entry.getValue());
+        }
     }
 
     @Override
--- src/homework12/Main.java
+++ src/homework12/Main.java
@@ -4,20 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        FileData fileData = new FileData("Homework", 10, "C:/Hillel/Lesson1");
-        FileData fileData2 = new FileData("Lessons", 15, "C:/Hillel/Lesson2");
-        FileData fileData3 = new FileData("Practice", 20, "C:/Practice/Homework1");
+        FileData fileData1 = new FileData("First Lesson", 10, "C:/Hillel/Lesson1");
+        FileData fileData2 = new FileData("Second Lesson", 20, "C:/Hillel/Lesson2");
+        FileData fileData3 = new FileData("Third Lesson", 30, "C:/Hillel/Lesson3");
+        FileData fileData4 = new FileData("First Homework", 15, "C:/Practice/Homework1");
 
         FileNavigator fileNavigator = new FileNavigator();
-        fileNavigator.add("C:/Hillel", fileData3);
+        fileNavigator.add("C:/Hillel", fileData1);
         fileNavigator.add("C:/Hillel", fileData2);
-        fileNavigator.add("C:/Practice", fileData);
+        fileNavigator.add("C:/Hillel", fileData3);
+        fileNavigator.add("C:/Practice", fileData4);
 
-        System.out.println("================================================\n"
-                + fileNavigator + "\n================================================");
+        System.out.println("================================================");
+        System.out.println("Method add working:");
+        System.out.println("================================================");
+        System.out.println(fileNavigator.fileStorage);
+        System.out.println(fileNavigator.fileStorage.get("C:/Hillel"));
+        System.out.println(fileNavigator.fileStorage.get("C:/Practice"));
 
-        fileNavigator.find("C:/Hillel");
+        System.out.println("\n================================================");
+        System.out.println("Method find working:");
+        System.out.println("================================================");
+        System.out.println(fileNavigator.find("C:/Hillel"));
+
+        System.out.println("\n================================================");
+        System.out.println("Method filterBySize working:");
+        System.out.println("================================================");
+        System.out.println(fileNavigator.filterBySize(11));
+
+        System.out.println("\n================================================");
+        System.out.println("Method remove working:");
+        fileNavigator.remove("C:/Hillel/Lesson2");
+        System.out.println("================================================");
 
     }
-
 }
